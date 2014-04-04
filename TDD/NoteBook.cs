730e75feef26c1027cc1a532e7beac770c27a046
Y132 <Y132@users.noreply.github.com>
2014-04-05 03:49:12 +0700

    Тест 17. Успех!

--- TDD/NoteBook.cs
+++ TDD/NoteBook.cs
@@ -7,17 +7,22 @@ namespace TDD
 {
     public class NoteBook
     {
+        protected
+            List<Note> notebook;
+
         public NoteBook()
         {
+            notebook = new List<Note>();
         }
         
         public int getCount()
         {
-            return 0;
+            return notebook.Count;
         }
 
         public void addNote(Note note)
         {
+            notebook.Add(note);
         }
     }
 }
